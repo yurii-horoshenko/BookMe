b6b2934ff26a9e91f490c6327c4f7c6a00a4d9d1
Yurii Goroshenko <ygoroshenko@griddynamics.com>
2024-03-16 15:04:49 +0100

    [Review] KMM removed unusable files

--- shared/src/androidMain/kotlin/com/gorosoft/bookme/now/Platform.android.kt
+++
@@ -1,7 +0,0 @@
-package com.gorosoft.bookme.now
-
-class AndroidPlatform : Platform {
-    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
-
-actual fun getPlatform(): Platform = AndroidPlatform()
