79c3e10297e342666c2ebc1d2fe798baebb25052
Yurii Goroshenko <ygoroshenko@griddynamics.com>
2024-04-06 23:10:16 +0200

    [Change] parsing response from KMM in iOS

--- iosApp/Source/Architecture/SplashView/SplashViewModel.swift
+++ iosApp/Source/Architecture/SplashView/SplashViewModel.swift
@@ -27,66 +27,12 @@ final class SplashViewModel: SplashViewModelProtocol {
     
     // MARK: - Public
     func detectPage() {
-        repository.login { result, error in
-//            result?.onSuccess(result: { <#ProfileModel#> in
-//                
-//            }).onError(action: { <#ResultWrapperError#> in
-//                <#code#>
-//            })
-
-//            switch result {
-//            case .success(let profile):
-//                printLog("How")
-//                
-//            case .failer(let error):
-//                printLog("How")
-//            }
-            
-            
-            
-            
-//            switch result {
-//            case is ResponseSuccess<ProfileModel>:
-//                let object = (result as? ResponseSuccess<ProfileModel>)?.value
-//                printLog("How")
-//                
-//            case is ResponseError:
-//                printLog("How")
-//                
-//            default:
-//                break
-//            }
+        repository.login { result, _ in
+            result?.onSuccess(result: { _ in
+                print("")
+            }).onError(result: { _, _ in
+                print("")
+            })
         }
     }
 }
-
-typealias SuccessClosure<T> = (T) -> Void
-
-//extension ResultWrapper<T> {
-//    @objc func onSuccess(result: (T) -> Void) -> Self {
-//        guard let object = (self as? ResultWrapperSuccess<AnyObject>)?.value else { return  self}
-////        result(object)
-//    }
-    
-//    func onError(result: (ResultWrapperError) -> Void) -> ResultWrapper<ProfileModel> {
-//        guard self is ResultWrapperError else { return self }
-//        guard let object = (self as? ResultWrapperError) else { return  self}
-//        result(object)
-//    }
-//}
-
-
-//extension ResultWrapper<ProfileModel> {
-//    func onSuccess(result: (ProfileModel) -> Void) -> ResultWrapper<ProfileModel> {
-//        guard self is ResultWrapperSuccess<ProfileModel> else { return self }
-//        guard let object = (self as? ResultWrapperSuccess<ProfileModel>)?.value else { return  self}
-//        result(object)
-//    }
-//    
-//    func onError(result: (ResultWrapperError) -> Void) -> ResultWrapper<ProfileModel> {
-//        guard self is ResultWrapperError else { return self }
-//        guard let object = (self as? ResultWrapperError) else { return  self}
-//        result(object)
-//    }
-//}
-//}
