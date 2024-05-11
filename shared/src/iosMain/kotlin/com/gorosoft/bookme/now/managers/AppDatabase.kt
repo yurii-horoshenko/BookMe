a33414e9eab34913631133bf93a1e5ce17a1c1c2
ahuminskyi <a.guminskiy@tranzzo.com>
2024-05-11 13:13:48 +0200

    Refactored.

--- shared/src/iosMain/kotlin/com/gorosoft/bookme/now/managers/AppDatabase.kt
+++ shared/src/iosMain/kotlin/com/gorosoft/bookme/now/managers/AppDatabase.kt
@@ -4,12 +4,13 @@ import androidx.room.Room
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 import com.gorosoft.bookme.now.data.database.AppDatabase
 import com.gorosoft.bookme.now.data.database.appDbFileName
+import com.gorosoft.bookme.now.data.database.instantiateImpl
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.IO
+import platform.Foundation.NSHomeDirectory
 
 fun createRoomDatabase(): AppDatabase {
-    val dbFile = "${fileDirectory()}/$appDbFileName"
-    // or val dbFile = NSHomeDirectory() + "/appDbFileName"
+    val dbFile = NSHomeDirectory() + "/$appDbFileName"
     return Room.databaseBuilder<AppDatabase>(
         name = dbFile,
         factory = { AppDatabase::class.instantiateImpl() }
