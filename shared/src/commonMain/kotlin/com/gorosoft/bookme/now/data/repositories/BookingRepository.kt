20877b320a5fb60e280b0fca6915a69443d6364f
Yurii Horoshenko <yurii.goroshenko@gmail.com>
2024-05-21 10:15:10 +0200

    [KMM] Add Booking responses (#28)
    Co-authored-by: Yurii Goroshenko <ygoroshenko@griddynamics.com>

--- shared/src/commonMain/kotlin/com/gorosoft/bookme/now/data/repositories/BookingRepository.kt
+++ shared/src/commonMain/kotlin/com/gorosoft/bookme/now/data/repositories/BookingRepository.kt
@@ -3,6 +3,7 @@ package com.gorosoft.bookme.now.data.repositories
 import com.gorosoft.bookme.now.Response
 import com.gorosoft.bookme.now.data.network.datasource.BookingRemoteDataSource
 import com.gorosoft.bookme.now.data.network.model.response.toDomain
+import com.gorosoft.bookme.now.domain.models.BookingModel
 import com.gorosoft.bookme.now.domain.models.PlaceModel
 import com.gorosoft.bookme.now.domain.repository.BookingRepositoryProtocol
 import com.gorosoft.bookme.now.map
@@ -13,7 +14,15 @@ class BookingRepository: BookingRepositoryProtocol, KoinComponent {
 
     private val remote: BookingRemoteDataSource by inject()
 
-    override suspend fun getBooking(count: Int): Response<PlaceModel> {
-        return remote.getPlaces(count = count).map { it.toDomain() }
+    override suspend fun getBooking(count: Int): Response<BookingModel> {
+        return remote.getBooking(count = count).map { it.toDomain() }
+    }
+
+    override suspend fun createBooking(place: PlaceModel, dataTime: Long): Response<Boolean> {
+        return remote.createBooking(place = place, dataTime = dataTime)
+    }
+
+    override suspend fun cancelBooking(bookingId: String): Response<Boolean> {
+        return remote.cancelBooking(bookingId = bookingId)
     }
 }
