1b5a568a197fa66b6413f27033621795ced0c0f0
Andrii <latasandrej@gmail.com>
2024-09-04 20:09:52 +0300

    Refactor code

--- androidApp/src/main/java/com/gorosoft/bookme/now/android/ui/main_screen/profile/LogoutBottom.kt
+++ androidApp/src/main/java/com/gorosoft/bookme/now/android/ui/main_screen/profile/LogoutBottom.kt
@@ -1,26 +1,15 @@
 package com.gorosoft.bookme.now.android.ui.main_screen.profile
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.Surface
-import androidx.compose.material.TabRowDefaults.Divider
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,51 +17,52 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
 import com.gorosoft.bookme.now.android.R
-import com.gorosoft.bookme.now.android.ui.main_screen.explore.EnableLocationContent
 import com.gorosoft.bookme.now.android.ui.theme.AppTheme
 import com.gorosoft.bookme.now.android.ui.utils.PrimaryButton
 import com.gorosoft.bookme.now.android.ui.utils.SecondaryButton
-import com.gorosoft.bookme.now.android.ui.utils.debounceClick
 
-@Preview
 @Composable
 fun Logout(
     modifier: Modifier = Modifier,
-    onDismiss: () -> Unit = { },
+    onLogout: () -> Unit = { },
+    onCancel: () -> Unit = { },
 ) {
 
-    Box(
-        modifier = Modifier
-            .width(428.dp)
-            .height(266.dp)
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
             .clip(RoundedCornerShape(topStart = 40.dp, topEnd = 40.dp))
             .background(AppTheme.colors.backgroundThemed.backgroundMain)
-            .border(1.dp, Color(0xF5F5F5))
+            .border(1.dp, Color(0xFFF5F5F5)),
+        horizontalAlignment = Alignment.CenterHorizontally,
 
 
-    ) {
+        ) {
+        HorizontalDivider(
+            modifier = Modifier
+                .width(38.dp)
+                .padding(top = 8.dp),
+            color = AppTheme.colors.grayscale.gs200,
+            thickness = 3.dp,
+        )
 
         Text(
             modifier = Modifier
-                .padding(40.dp)
-                .align(Alignment.TopCenter),
-            text = "Logout",
+                .padding(top = 24.dp),
+            text = stringResource(R.string.logout),
             style = AppTheme.typography.heading.h4,
-            color = Color(0xFFF75555)
+            color = AppTheme.colors.alertStatus.error
+
         )
         HorizontalDivider(
-            modifier =Modifier
-                .align(Alignment.TopCenter)
+            modifier = Modifier
                 .width(380.dp)
-                .padding(top = 95.dp)
-            ,
+                .padding(top = 24.dp, start = 24.dp, end = 24.dp),
             color = AppTheme.colors.grayscale.gs200,
             thickness = 1.dp,
         )
@@ -80,42 +70,45 @@ fun Logout(
 
         Text(
             modifier = Modifier
-                .align(Alignment.Center),
-            text = "Are you sure you want to log out?",
+                .padding(top = 24.dp),
+            text = stringResource(R.string.are_you_sure_you_want_to_log_out),
             style = AppTheme.typography.heading.h5,
-            color = Color.Black,
+            color = AppTheme.colors.grayscale.gs800,
         )
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .clip(RoundedCornerShape(20.dp))
-                .padding(24.dp)
-                .align(Alignment.BottomCenter)
+                .padding(top = 24.dp, start = 24.dp, end = 24.dp, bottom = 48.dp),
+            horizontalArrangement = Arrangement.spacedBy(12.dp)
 
 
         ) {
             SecondaryButton(
                 modifier = Modifier
-                    .padding(top = 32.dp, start = 1.dp, end = 3.dp)
                     .fillMaxWidth()
                     .weight(1f),
                 text = stringResource(R.string.cancel),
-                onClick = {},
+                onClick = onCancel,
             )
             PrimaryButton(
                 modifier = Modifier
-                    .padding(top = 32.dp, start = 1.dp, end = 3.dp)
                     .fillMaxWidth()
                     .weight(1f),
-                text = "Yes, Logout",
-                onClick = {},
+                text = stringResource(R.string.yes_logout),
+                onClick = onLogout,
             )
-
         }
-
     }
 }
 
+@Preview
+@Composable
+private fun LogoutPreview() {
+    AppTheme {
+        Logout()
+    }
+}
 
 
 
