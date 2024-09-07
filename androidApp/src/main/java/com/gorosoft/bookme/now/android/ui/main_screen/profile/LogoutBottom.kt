66808172e69183afe67a72afe032f33cadbd8d20
Andrii <latasandrej@gmail.com>
2024-09-07 17:25:47 +0300

    refactor code

--- androidApp/src/main/java/com/gorosoft/bookme/now/android/ui/main_screen/profile/LogoutBottom.kt
+++ androidApp/src/main/java/com/gorosoft/bookme/now/android/ui/main_screen/profile/LogoutBottom.kt
@@ -1,13 +1,10 @@
 package com.gorosoft.bookme.now.android.ui.main_screen.profile
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
@@ -16,7 +13,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
