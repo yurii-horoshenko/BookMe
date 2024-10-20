d76d2900ac5638446bd0a20c47e805ffa100dbe5
Латаш Андрій <91695117+AndriiLatash@users.noreply.github.com>
2024-10-21 00:29:08 +0300

    [Android] Added Edit profile screen (#41)
    * refactor code
    
    * Created edit profile screen, added navigation
    
    * refactor code
    
    * added email validation
    
    * added viewModel
    
    * added scrollbar
    
    * fixed viewModel
    
    * merged develop
    
    * refactored
    
    * refactor code
    
    * fixed icon
    
    * code refactor, fixed focusManager
    
    * check code

---
+++ androidApp/src/main/java/com/gorosoft/bookme/now/android/ui/main_screen/profile/edit_profile/EditProfileViewModel.kt
@@ -0,0 +1,50 @@
+package com.gorosoft.bookme.now.android.ui.main_screen.profile.edit_profile
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.gorosoft.bookme.now.android.ui_models.EditProfileUiModel
+import com.gorosoft.bookme.now.domain.models.UserGenderType
+import java.time.LocalDate
+
+private const val EditProfileKey = "EditYourProfile"
+
+class EditProfileViewModel(
+    private val savedStateHandle: SavedStateHandle,
+) : ViewModel() {
+
+    private val state get() = profileState.value
+
+    val profileState = savedStateHandle.getStateFlow(EditProfileKey, EditProfileUiModel())
+
+    fun updateName(newFirstName: String) {
+        updateState { copy(firstName = newFirstName) }
+    }
+
+    fun updateSecondName(newSecondName: String) {
+        updateState { copy(secondName = newSecondName) }
+    }
+
+//    fun updatePhone(phone: String) {
+//        updateState { copy(phone = phone) }
+//    }
+
+    fun updateEmail(email: String) {
+        updateState { copy(email = email) }
+    }
+
+    fun updateAddress(address: String) {
+        updateState { copy(address = address) }
+    }
+
+    fun updateGender(gender: UserGenderType) {
+        updateState { copy(gender = gender) }
+    }
+
+    fun updateDateOfBirth(date: LocalDate) {
+        updateState { copy(dateOfBirthDate = date) }
+    }
+
+    private fun updateState(action: EditProfileUiModel.() -> EditProfileUiModel) {
+        savedStateHandle[EditProfileKey] = action.invoke(state)
+    }
+}
